7c9358a8af8b536b9d0de7ff1b72368a6015b304
JavierSG <jsanudo@gteam.es>
2021-09-02 12:35:06 +0200

    Agregar archivos de proyecto.

---
+++ BancoJavi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BancoJavi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
